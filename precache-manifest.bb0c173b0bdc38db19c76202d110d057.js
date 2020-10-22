2fe4687242765871680a2bfb12eb4ec64e65b7f4
Sweat <upsidedownplatypus@gmail.com>
2020-10-22 14:03:44 -0400

    Updates

---
+++ precache-manifest.bb0c173b0bdc38db19c76202d110d057.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "852b3a1ec2ad60816299",
+    "url": "/static/css/main.962adfb0.chunk.css"
+  },
+  {
+    "revision": "852b3a1ec2ad60816299",
+    "url": "/static/js/main.852b3a1e.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "a8f24be1d624f87ca3e27e0867051e38",
+    "url": "/index.html"
+  }
+];
